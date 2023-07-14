46991d19c7e5a5416f19e0b7d23779d606a52409
Pell305 <cowpvest@gmail.com>
2023-07-14 16:39:39 -0700

    Add 2-print_alphabet.c file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,10 @@
-#include <stdio.h>
+#include <iostream>
 
-/**
- * main - Prints the alphabet in lowercase, and then in uppercase.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char letter;
+int main() {
+    for (char c = 'a'; c <= 'z'; ++c) {
+        putchar(c);
+    }
+    putchar('\n');
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-
-	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-
-	putchar('\n');
-
-	return (0);
+    return 0;
 }
