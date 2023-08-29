7b37ce44282e4ca6de19118ac663ba69cdbd0f9a
Pell305 <cowpvest@gmail.com>
2023-08-29 04:54:42 -0700

    Add 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,72 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_safe - Frees a listint_t linked list.
- * @h: A pointer to the pointer of the head node.
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
  *
- * Return: The size of the list that was freed.
+ * Return: no return.
+ */
+void free_listp2(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *current = *h;
-	listint_t *next_node = NULL;
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+	listint_t *curr;
 
-	while (current != NULL)
+	hptr = NULL;
+	while (*h != NULL)
 	{
-		count++;
-		next_node = current->next;
-		free(current);
-		current = next_node;
+		new = malloc(sizeof(listp_t));
 
-		/* If the next node is revisited, it's a loop; break it */
-		if (next_node == *h)
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
 		{
-			*h = NULL;
-			break;
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_listp2(&hptr);
+				return (nnodes);
+			}
 		}
+
+		curr = *h;
+		*h = (*h)->next;
+		free(curr);
+		nnodes++;
 	}
 
-	return (count);
+	*h = NULL;
+	free_listp2(&hptr);
+	return (nnodes);
 }
