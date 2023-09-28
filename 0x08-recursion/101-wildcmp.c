592d0378e70d7bc96af4e953888b919a0ef6afe4
Pell305 <cowpvest@gmail.com>
2023-09-28 16:15:58 -0700

    Add 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,13 +14,10 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '\0')
 			return (1); /* The pattern * matches any string. */
 
-		while (*s1)
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1); /* Recursively match rest of the strings. */
-			s1++;
-		}
-		return (0); /* No matching substring found. */
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1)); /* Skip one character in s2. */
+
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 
 	if (*s1 == *s2)
